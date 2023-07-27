31eabf6c731cb4fe8a92a6cde88ea6b2513c9e71
diego64 <diegoferreira1964@gmail.com>
2023-07-27 06:23:44 -0300

    Desenvolvimento da lista de atividades

---
+++ src/components/Task/index.tsx
@@ -0,0 +1,44 @@
+import { TouchableOpacity, View, Text } from 'react-native';
+import { MaterialCommunityIcons } from '@expo/vector-icons'
+
+import { theme } from '../../theme';
+import { styles } from './styles';
+import { TaskDTO } from '../../dtos/TaskDTO';
+
+export function Task({ title, isCompleted}: TaskDTO) {
+  return (
+    <View style={styles.taskContainer}>
+      <TouchableOpacity>
+        <MaterialCommunityIcons
+          name={
+            isCompleted
+            ? 'checkbox-marked-circle-outline'
+            : 'checkbox-blank-circle-outline'
+          }
+          size={22}
+          color={
+            isCompleted
+            ? theme.colors.brand.purple
+            : theme.colors.brand.blue
+          }
+        />
+      </TouchableOpacity>
+
+      <View style={styles.textContainer}>
+        <Text
+          style={isCompleted ? styles.textDone : styles.textCreated}
+        >
+          {title}
+        </Text>
+      </View>
+
+      <TouchableOpacity>
+        <MaterialCommunityIcons 
+          name="trash-can-outline"
+          size={22}
+          color={theme.colors.base.gray300}
+        />
+      </TouchableOpacity>
+    </View>
+  )
+}
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,49 @@
-import { View, Text } from 'react-native';
+import { View, Text, FlatList } from 'react-native';
+import { useState } from 'react';
+
 import { styles } from './styles';
+
 import { Header } from '../../components/Header';
+import { Task } from '../../components/Task';
+import { Empty } from '../../components/Empty';
+
+import { TaskDTO } from '../../dtos/TaskDTO';
 
 export function Home() {
+  const [tasks, setTasks] = useState<TaskDTO[]>([])
+
   return(
     <View style={styles.container}>
       <Header />
+      <View style={styles.tasksTitle}>
+        <View style={styles.info}>
+          <View style={styles.row}>
+            <Text style={styles.tasksCreated}>Criadas</Text>
+            <View style={styles.counterContainer}>
+              <Text style={styles.counterText}>0</Text>
+            </View>
+          </View>
+          <View style={styles.row}>
+            <Text style={styles.tasksDone}>Concluídas</Text>
+            <View style={styles.counterContainer}>
+              <Text style={styles.counterText}>0</Text>
+            </View>
+          </View>
+        </View>
+
+        <FlatList 
+          data={tasks}
+          keyExtractor={(tasks) => tasks.id!}
+          renderItem={({ item }) => (
+            <Task 
+              key={item.id}
+              isCompleted={item.isCompleted}
+              title={item.title}
+            />
+          )}
+          ListEmptyComponent={<Empty />}
+        />
+      </View>
     </View>
   )
 }
